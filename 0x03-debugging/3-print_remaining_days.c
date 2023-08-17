fade2d2acc34366b8f8cd35352810bd4269454c2
barcelsway <barcelsway@gmail.com>
2023-08-17 02:19:52 -0700

    fixing

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,7 +11,7 @@
 void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
-	
+
 	{
 		if (month > 2 && day >= 60)
 		{
@@ -31,5 +31,5 @@ void print_remaining_days(int month, int day, int year)
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 		}
-	}	
+	}
 }

4b68b5108dbd9d6d5eaf9129a9d61409ed7f9e13
barcelsway <barcelsway@gmail.com>
2023-09-28 11:03:10 -0700

    corrected

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,26 +1,25 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts any binary No. to unsighed int
- * b: string with binary No. 
+ * binary_to_uint - Converts a binary string to an unsigned integer.
+ * @b: String containing the binary number.
  *
- * Return: the converted No.
+ * Return: The converted number, or 0 if an error occurs.
  */
 unsigned int binary_to_uint(const char *b)
 {
-        int a;
-        unsigned int dec_val = 0;
+	int a;
+	unsigned int dec_val = 0;
 
-        if (!b)
-                return (0);
+	if (!b)
+		return (0);
 
-        for (a = 0; b[a]; a++)
-        {
-                if (b[a] < '0' || b[a] > '1')
-                        return (0);
-                dec_val = 2 * dec_val + (b[a] - '0');
+	for (a = 0; b[a]; a++)
+	{
+		if (b[a] < '0' || b[a] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[a] - '0');
+	}
 
-        }
-
-        return (dec_val);
+	return (dec_val);
 }
--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
  * _putchar - prints the char c to stdout
- * c: The char to printed
+ *@c: The char to printed
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
-        return (write(1, &c, 1));
+	return (write(1, &c, 1));

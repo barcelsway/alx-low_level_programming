475ee8415fad59125fa7b229413111faf814f4de
barcelsway <barcelsway@gmail.com>
2023-10-02 13:21:47 -0700

    append text to file corrected

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -24,7 +24,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		for (let = 0; text_content[let]; let)
+		for (let = 0; text_content[let]; let++)
 			;
 
 		w = write(fd, text_content, let);
